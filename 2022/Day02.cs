f7a426417c042328e003f49d6a2aa13f424db443
Kieran Harris <kieran@fundapps.co>
2022-12-03 23:46:08 +0000

    Day2

---
+++ 2022/Day02.cs
@@ -0,0 +1,38 @@
+﻿namespace AdventOfCode._2022;
+
+public class Day02: Solution<IEnumerable<string>, int>
+{
+    public override int Part1()
+    {
+        var lookup = new Dictionary<string, int>
+        {
+            { "A X", 4 },
+            { "A Y", 8 },
+            { "A Z", 3 },
+            { "B X", 1 },
+            { "B Y", 5 },
+            { "B Z", 9 },
+            { "C X", 7 },
+            { "C Y", 2 },
+            { "C Z", 6 },
+        };
+        return Input.Sum(x => lookup[x]);
+    }
+
+    public override int Part2()
+    {
+        var lookup = new Dictionary<string, int>
+        {
+            { "A X", 3 },
+            { "A Y", 4 },
+            { "A Z", 8 },
+            { "B X", 1 },
+            { "B Y", 5 },
+            { "B Z", 9 },
+            { "C X", 2 },
+            { "C Y", 6 },
+            { "C Z", 7 },
+        };
+        return Input.Sum(x => lookup[x]);
+    }
+}
