b7d249d861a34e98cc23c47d99cfe49c05ad2add
Kieran Harris <kieran@fundapps.co>
2022-12-03 23:45:53 +0000

    Day1

---
+++ 2022/Day01.cs
@@ -0,0 +1,17 @@
+﻿namespace AdventOfCode._2022;
+
+public class Day01: Solution<string, int>
+{
+    private IEnumerable<int> Calories => Input.Split("\n\n")
+        .Select(x => x.Split("\n").Select(int.Parse).Sum());
+    
+    public override int Part1()
+    {
+        return Calories.Max();
+    }
+
+    public override int Part2()
+    {
+        return Calories.OrderByDescending(x => x).Take(3).Sum();
+    }
+}
