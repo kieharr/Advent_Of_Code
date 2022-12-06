08e75bb8e363da1e2cf1dfcb9b912576f3987898
Kieran Harris <kieran@fundapps.co>
2022-12-06 19:17:21 +0000

    Day 6

---
+++ AdventOfCode/2022/Day06.cs
@@ -0,0 +1,29 @@
+﻿namespace AdventOfCode._2022;
+
+public class Day06: Solution<string, int>
+{ 
+    protected override int Part1(string input)
+    {
+        return GetMarker(input, 4);
+    }
+
+    protected override int Part2(string input)
+    {
+        return GetMarker(input, 14);
+    }
+
+    private static int GetMarker(string input, int distinctCount)
+    {
+        var recent = input.Take(distinctCount).ToList();
+        for (var i = distinctCount; i < input.Length; i++)
+        {
+            if (recent.Distinct().Count() == distinctCount)
+            {
+                return i;
+            }
+            recent.RemoveAt(0);
+            recent.Add(input[i]);
+        }
+        throw new Exception("Marker not found");
+    }
+}
---
+++ AdventOfCodeTests/Day06Tests.cs
@@ -0,0 +1,56 @@
+﻿using AdventOfCode._2022;
+
+namespace AdventOfCodeTests;
+
+public class Day06Tests
+{
+        [TestCase("mjqjpqmgbljsphdztnvjfqwrcgsmlb", 7)]
+        [TestCase("bvwbjplbgvbhsrlpgdmjqwftvncz", 5)]
+        [TestCase("nppdvjthqldpwncqszvftbrmjlhg", 6)]
+        [TestCase("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg", 10)]
+        [TestCase("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw", 11)]
+        public void TestPart1(string input, int expected)
+        {
+            var sut = new Day06();
+            
+            var result = sut.RunPart1(input);
+            
+            Assert.That(result, Is.EqualTo(expected));
+        }
+    
+        [Test]
+        public void TestPart1_Solution()
+        {
+            var sut = new Day06();
+            var input = sut.GetInput();
+            
+            var result = sut.RunPart1(input);
+            
+            Assert.That(result, Is.EqualTo(1300));
+        }
+        
+        [TestCase("mjqjpqmgbljsphdztnvjfqwrcgsmlb", 19)]
+        [TestCase("bvwbjplbgvbhsrlpgdmjqwftvncz", 23)]
+        [TestCase("nppdvjthqldpwncqszvftbrmjlhg", 23)]
+        [TestCase("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg", 29)]
+        [TestCase("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw", 26)]
+        public void TestPart2(string input, int expected)
+        {
+            var sut = new Day06();
+            
+            var result = sut.RunPart2(input);
+            
+            Assert.That(result, Is.EqualTo(expected));
+        }
+    
+        [Test]
+        public void TestPart2_Solution()
+        {
+            var sut = new Day06();
+            var input = sut.GetInput();
+            
+            var result = sut.RunPart2(input);
+            
+            Assert.That(result, Is.EqualTo(3986));
+        }
+}
